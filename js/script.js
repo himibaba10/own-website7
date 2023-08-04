5578a2b1e901866c6a84da58e1d7633d6d035989
himibaba10 <himibaba10@gmail.com>
2023-08-05 03:08:05 +0600

    added scroll-to-top button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// For navbar
 const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
   const scrolled = window.scrollY;
@@ -11,4 +12,27 @@ window.addEventListener("scroll", function () {
       header.classList.remove("sticky");
     }
   }
+});
+
+// For scroll-to-top
+const scrollToTopBtn = document.querySelector(".scroll-to-top");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 150) {
+    scrollToTopBtn.style.display = "block";
+    this.setTimeout(() => {
+      scrollToTopBtn.style.opacity = "1";
+    }, 100);
+  } else {
+    scrollToTopBtn.style.opacity = "0";
+    () =>
+      this.setTimeout(() => {
+        scrollToTopBtn.style.display = "none";
+      }, 500);
+  }
+});
+
+scrollToTopBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
